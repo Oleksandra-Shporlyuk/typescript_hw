5f986f9add95c55284866ae260751845b23d23aa
Oleksandra-Shporlyuk <ssshporlyuk@gmail.com>
2023-04-05 17:21:00 +0300

    homework.typescript

--- homework.ts
+++ homework.ts
@@ -1,41 +1,41 @@
 
 // 1) створити інтерфейс на основі цього объекта:
 //     Зверніть увагу там де масиви... в них може бути багато однотипних обїектів
-interface ElonMusk {
-    mission_name: string,
-    launch_date_local: string,
-    launch_site: {
-        site_name_long: string
-    },
-
-    links: {
-        article_link: string
-        video_link: string
-    },
-    rocket: {
-        rocket_name: string,
-        first_stage: {
-            cores: [
-                {
-                    flight: number
-                    core: {
-                        reuse_count: number
-                        status: string
-                    }
-                }
-            ]
-        },
-        second_stage: {
-            payloads: [
-                {
-                    payload_type: string
-                    payload_mass_kg: number
-                    payload_mass_lbs: number
-                }
-            ]
-        }
-    }
-}
+// interface ElonMusk {
+//     mission_name: string,
+//     launch_date_local: string,
+//     launch_site: {
+//         site_name_long: string
+//     },
+//
+//     links: {
+//         article_link: string
+//         video_link: string
+//     },
+//     rocket: {
+//         rocket_name: string,
+//         first_stage: {
+//             cores: [
+//                 {
+//                     flight: number
+//                     core: {
+//                         reuse_count: number
+//                         status: string
+//                     }
+//                 }
+//             ]
+//         },
+//         second_stage: {
+//             payloads: [
+//                 {
+//                     payload_type: string
+//                     payload_mass_kg: number
+//                     payload_mass_lbs: number
+//                 }
+//             ]
+//         }
+//     }
+// }
 
 // 2) протипізувати функції:
 
@@ -45,9 +45,9 @@ interface ElonMusk {
 //     age:18,
 //     gender:'male'
 // }
-    const user = (name:string,age:number,gender:string) => {
+    const users = (name:string,age:number,gender:string) => {
     }
-    user('Max',18,'male');
+    users('Max',18,'male');
 
 
 
@@ -55,7 +55,7 @@ interface ElonMusk {
 // function sum(a,b){
 //     return a+b
 // }
-const sum = (a:number,b:number) => {
+const sum = (a:number,b:number): number => {
     return a+b;
 }
 console.log(sum(1, 2));
@@ -69,7 +69,7 @@ console.log(sum(1, 2));
     const showSum = (a:number,b:number) =>  {
         console.log(a+b);
     }
-console.log(showSum(2, 3));
+showSum(2, 3);
 
 
 //////////// ЧЕТВЕРТА
@@ -80,12 +80,31 @@ console.log(showSum(2, 3));
 
 // const incAge = (someUSer:string, inc:number) => {
 //
+//     class User {
+//         public age:number
 //
-//     someUSer = {
-//         age:18;
+//         constructor(age: number) {
+//             this.age = age;
+//         }
+//         getAge() {
+//             return this.age;
+//         }
 //     }
+//     new User(2) ;
 //
-//     someUSer.age+=inc;
 //     return someUSer;
 // }
 // incAge('user',2);
+
+interface User {
+    age:number;
+}
+
+const incAge = (someUser:User,inc:number)  =>   {
+    someUser.age+=inc;
+    return someUser;
+}
+const user: User = {age:18};
+
+incAge(user,2);
+console.log(user);
